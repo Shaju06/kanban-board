b82af8afc44f380a418fc0dcd86c023bfeb017cf
= <=>
2024-05-05 19:54:13 +0530

    chore: formated code and added tasks count

--- app/components/KandanBoard.tsx
+++ app/components/KandanBoard.tsx
@@ -1,184 +1,204 @@
-"use client"
+"use client";
 
 import { useState, useMemo, useEffect } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 import PlusIcon from "./Icons/PlusIcon";
 import { Container, Id } from "@/Types/Column";
-import {DndContext, DragEndEvent, DragStartEvent, PointerSensor, KeyboardSensor, useSensor, useSensors} from '@dnd-kit/core';
-import { SortableContext, arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
+import {
+  DndContext,
+  DragEndEvent,
+  DragStartEvent,
+  PointerSensor,
+  KeyboardSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  arrayMove,
+  sortableKeyboardCoordinates,
+} from "@dnd-kit/sortable";
 import ColumnContainer from "./ColumnContainer";
 import Modal from "./Modal";
 import { getContainer, isSameContainer } from "@/utils";
 
 const KandanBoard = () => {
+  const [containers, setContainers] = useState<Container[]>([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [activeId, setActiveId] = useState<Id | null>(null);
+  const colsId = useMemo(() => containers.map((item) => item.id), [containers]);
 
-    const [containers, setContainers] = useState<Container[]>([])
-    const [openModal, setOpenModal] = useState(false);
-    const [activeId, setActiveId] = useState<Id | null>(null);
-    const colsId = useMemo(()=> containers.map((item)=> item.id),[containers])
-
-    const sensors = useSensors(useSensor(PointerSensor, {
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
       activationConstraint: {
         distance: 10,
-      }
+      },
     }),
     useSensor(KeyboardSensor, {
       coordinateGetter: sortableKeyboardCoordinates,
-    }),
-    )
+    })
+  );
 
-    const createNewCol = (enteredVal: string) => {
-        const containerToAdd: Container = {
-            id: uuidv4(),
-            title: enteredVal,
-            taskItems: []
-        }
-        setContainers(prev => [...prev, containerToAdd])
-    }
+  const createNewCol = (enteredVal: string) => {
+    const containerToAdd: Container = {
+      id: uuidv4(),
+      title: enteredVal,
+      taskItems: [],
+    };
+    setContainers((prev) => [...prev, containerToAdd]);
+  };
 
-    const onDragStart = (event: DragStartEvent) => {
-      const { active } = event;
-      const { id } = active;
-      setActiveId(id);
-    }
+  const onDragStart = (event: DragStartEvent) => {
+    const { active } = event;
+    const { id } = active;
+    setActiveId(id);
+  };
 
-    const onModalClose = (value: string) => {
-      value !== '' ?  createNewCol(value) : null
-      setOpenModal(false)
-    }
+  const onModalClose = (value: string) => {
+    value !== "" ? createNewCol(value) : null;
+    setOpenModal(false);
+  };
 
-    const handleDragEnd = (event: DragEndEvent) => {
-      const { active, over } = event;
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
 
-      console.log(active, over, 'fsfsf')
+    console.log(active, over, "fsfsf");
 
-      if (!over) return;
-      const activeId = active.id;
-      const overId = over.id;
-  
-      if (activeId === overId) return;
+    if (!over) return;
+    const activeId = active.id;
+    const overId = over.id;
 
-      const isActiveAColumn = active.data.current?.type === "Container";
-      const isActiveItem = active.data.current?.type === "Task";
+    if (activeId === overId) return;
 
-      if(isActiveAColumn) {
-        setContainers((columns) => {
-          const activeColumnIndex = columns.findIndex((col) => col.id === activeId);
-    
-          const overColumnIndex = columns.findIndex((col) => col.id === overId);
-    
-          return arrayMove(containers, activeColumnIndex, overColumnIndex);
-        });
-      } else {
-          const isContainerSame = isSameContainer(active, over)
-         
+    const isActiveAColumn = active.data.current?.type === "Container";
+    const isActiveItem = active.data.current?.type === "Task";
 
-          if(isContainerSame ) {
-            const container = getContainer(active.id, 'Task', containers)
-            if(!container) return 
+    if (isActiveAColumn) {
+      setContainers((columns) => {
+        const activeColumnIndex = columns.findIndex(
+          (col) => col.id === activeId
+        );
 
-            const oldIndex = container.taskItems.findIndex( item => item.id === active.id);
-            const newIndex = container.taskItems.findIndex( item => item.id === over.id);
+        const overColumnIndex = columns.findIndex((col) => col.id === overId);
 
-            const activeContainerIndex = containers.findIndex((cont)=> cont.id === container.id)
+        return arrayMove(containers, activeColumnIndex, overColumnIndex);
+      });
+    } else {
+      const isContainerSame = isSameContainer(active, over);
 
-            const newObj = [...containers]
+      if (isContainerSame) {
+        const container = getContainer(active.id, "Task", containers);
+        if (!container) return;
 
-            newObj[activeContainerIndex].taskItems = arrayMove(container.taskItems, oldIndex, newIndex);
+        const oldIndex = container.taskItems.findIndex(
+          (item) => item.id === active.id
+        );
+        const newIndex = container.taskItems.findIndex(
+          (item) => item.id === over.id
+        );
 
-            container.taskItems.map((column)=> {
-              if(column.id === container.id) {
-                return 
-              } return column
-            })
-            setContainers(newObj)
-          } else {
-            const activeContainer = getContainer(active.id, 'Task', containers);
-            const overContainer = getContainer(over.id, 'Container', containers);
+        const activeContainerIndex = containers.findIndex(
+          (cont) => cont.id === container.id
+        );
 
-            if (!activeContainer || !overContainer) return;
+        const newObj = [...containers];
 
-            const activeContainerIndex = containers.findIndex(
-              (container) => container.id === activeContainer.id,
-            );
-            const overContainerIndex = containers.findIndex(
-              (container) => container.id === overContainer.id,
-            );
+        newObj[activeContainerIndex].taskItems = arrayMove(
+          container.taskItems,
+          oldIndex,
+          newIndex
+        );
 
-            const activeitemIndex = activeContainer.taskItems.findIndex(
-              (item) => item.id === active.id,
-            );
-      
-            // Remove the active item from the active container and add it to the over container
-            let newItems = [...containers];
-            const [removeditem] = newItems[activeContainerIndex].taskItems.splice(
-              activeitemIndex,
-              1,
-            );
-            newItems[overContainerIndex].taskItems.push(removeditem);
-            setContainers(newItems);
-
-
+        container.taskItems.map((column) => {
+          if (column.id === container.id) {
+            return;
           }
-
-
-      }
-          
-    }
-
-
-    const removeColumn = (colId: Id) => {
-        const filteredCol = containers.filter( item => item.id !== colId)
-        setContainers(filteredCol)
-    }
-
-    useEffect(() => {
-
-      if(openModal) {
-        document.body.classList.add('modal-open');
-
+          return column;
+        });
+        setContainers(newObj);
       } else {
-        document.body.classList.remove('modal-open');
+        const activeContainer = getContainer(active.id, "Task", containers);
+        const overContainer = getContainer(over.id, "Container", containers);
+
+        if (!activeContainer || !overContainer) return;
+
+        const activeContainerIndex = containers.findIndex(
+          (container) => container.id === activeContainer.id
+        );
+        const overContainerIndex = containers.findIndex(
+          (container) => container.id === overContainer.id
+        );
+
+        const activeitemIndex = activeContainer.taskItems.findIndex(
+          (item) => item.id === active.id
+        );
+
+        // Remove the active item from the active container and add it to the over container
+        let newItems = [...containers];
+        const [removeditem] = newItems[activeContainerIndex].taskItems.splice(
+          activeitemIndex,
+          1
+        );
+        newItems[overContainerIndex].taskItems.push(removeditem);
+        setContainers(newItems);
       }
+    }
+  };
 
-    },[openModal])
+  const removeColumn = (colId: Id) => {
+    const filteredCol = containers.filter((item) => item.id !== colId);
+    setContainers(filteredCol);
+  };
 
+  useEffect(() => {
+    if (openModal) {
+      document.body.classList.add("modal-open");
+    } else {
+      document.body.classList.remove("modal-open");
+    }
+  }, [openModal]);
 
-    return (
-        <div className="m-auto flex flex-col ">
-        <div className="flex justify-between static items-center w-full m-10">
-            <h2 className="text-lg font-semibold">Kanban Board Demo</h2>
-         <div
-         className='mr-20'
-         >
-         <button
+  return (
+    <div className="m-auto flex flex-col ">
+      <div className="flex justify-between static items-center w-full m-10">
+        <h2 className="text-lg font-semibold">Kanban Board Demo</h2>
+        <div className="mr-20">
+          <button
             onClick={() => setOpenModal(true)}
             className="h-[60px] w-[350px] min-w-[350px] rounded-lg bg-mainBgColor p-4 border-2 ring-rose-400 hover:ring-2 border-colBgColor cursor-pointer flex items-center gap-1 mr-4"
           >
-            <PlusIcon  />
-            <span className="flex items-center">Add Column</span> 
+            <PlusIcon />
+            <span className="flex items-center">Add Column</span>
           </button>
-         </div>
         </div>
-      <DndContext sensors={sensors} onDragEnd={handleDragEnd} onDragStart={onDragStart}>
-        <div className="m-auto flex gap-4   overflow-x-auto
-        overflow-y-hidden">
+      </div>
+      <DndContext
+        sensors={sensors}
+        onDragEnd={handleDragEnd}
+        onDragStart={onDragStart}
+      >
+        <div
+          className="m-auto flex gap-4   overflow-x-auto
+        overflow-y-hidden"
+        >
           <div className="flex gap-4">
             <SortableContext items={colsId}>
-            {containers.map((container) => (
-              <ColumnContainer key={container.id} removeColumn={removeColumn} setContainers={setContainers} container={container} />
-            ))}
+              {containers.map((container) => (
+                <ColumnContainer
+                  key={container.id}
+                  removeColumn={removeColumn}
+                  setContainers={setContainers}
+                  container={container}
+                />
+              ))}
             </SortableContext>
           </div>
         </div>
-        </DndContext>
-        {
-          openModal ? <Modal onClose={onModalClose} /> : null
-        }
-      </div>
-      
-    );
-}
+      </DndContext>
+      {openModal ? <Modal onClose={onModalClose} /> : null}
+    </div>
+  );
+};
 
 export default KandanBoard;
--- app/components/TaskItem.tsx
+++ app/components/TaskItem.tsx
@@ -1,109 +1,107 @@
-import { Container } from '@/Types/Column';
-import {useState, useEffect} from 'react'
-import { useSortable } from '@dnd-kit/sortable';
-import { CSS } from '@dnd-kit/utilities';
+import { Container } from "@/Types/Column";
+import { useState, useEffect } from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface TaskProps {
-  container: Container
-  setContainers: any
-  taskId: string | number
+  container: Container;
+  setContainers: any;
+  taskId: string | number;
 }
 
 const TaskItem = (props: TaskProps) => {
+  const { container, taskId, setContainers } = props;
+  const [taskTitle, setTaskTitle] = useState("");
 
-    const {container, taskId, setContainers} = props
-    const [taskTitle, setTaskTitle] = useState('')
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: taskId,
+    data: {
+      type: "Task",
+    },
+  });
 
-    const {
-      attributes,
-      listeners,
-      setNodeRef,
-      transform,
-      transition,
-      isDragging,
-    } = useSortable({
-      id: taskId,
-      data: {
-        type: 'Task',
-      },
-    });
+  function debounce<T extends (...args: any[]) => void>(
+    func: T,
+    timeout = 300
+  ) {
+    let timer: NodeJS.Timeout;
 
+    return (...args: Parameters<T>) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func.bind(null, ...args);
+      }, timeout);
+    };
+  }
 
-    function debounce<T extends (...args: any[]) => void>(func: T, timeout = 300) {
-      let timer: NodeJS.Timeout;
-    
-      return (...args: Parameters<T>) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-          func.call(this, ...args);
-        }, timeout);
-      };
-    }
-
-   function setTask(arg: string) {
-  console.log(`Function called with argument: ${arg}`);
-  container.taskItems.map((item)=> {
-    if(item.id === taskId) {
-      return {
-        ...item,
-        title: arg
-      }
-    }
-    return item
-  })
-  setContainers((prev: Container[])=> {
-    console.log()
-
-   return  prev.map((cont: Container)=> {
-      if(container.id === cont.id) {
-     const subTask =  cont.taskItems.map((task)=> {
-          if(task.id === taskId) {
-            return {
-              ...task,
-              title: arg
-            }
-          }
-          return task
-        })
-
+  function setTask(arg: string) {
+    console.log(`Function called with argument: ${arg}`);
+    container.taskItems.map((item) => {
+      if (item.id === taskId) {
         return {
-          ...cont,
-          subTask
-        }
+          ...item,
+          title: arg,
+        };
       }
-      return cont
-    })
-  })
-}
+      return item;
+    });
+    setContainers((prev: Container[]) => {
+      return prev.map((cont: Container) => {
+        if (container.id === cont.id) {
+          const subTask = cont.taskItems.map((task) => {
+            if (task.id === taskId) {
+              return {
+                ...task,
+                title: arg,
+              };
+            }
+            return task;
+          });
 
-useEffect(() => {
-  const fnc = debounce(setTask, 500)
-  fnc(taskTitle)
-},[taskTitle])
+          return {
+            ...cont,
+            subTask,
+          };
+        }
+        return cont;
+      });
+    });
+  }
 
-    return (  
-        <div 
-        ref={setNodeRef}
-        style={{
-          transition,
-          transform: CSS.Translate.toString(transform),
+  useEffect(() => {
+    const fnc = debounce(setTask, 500);
+    fnc(taskTitle);
+  }, [taskTitle]);
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={{
+        transition,
+        transform: CSS.Translate.toString(transform),
+      }}
+      {...attributes}
+      {...listeners}
+      className="flex items-center mb-2"
+    >
+      <textarea
+        value={taskTitle}
+        autoFocus
+        onChange={(e) => {
+          // setContainers
+          setTaskTitle(e.target.value);
         }}
-        {...attributes}
-        {...listeners}
-        className="flex items-center mb-2">
-          <input
-            type="text"
-            value={taskTitle}
-            autoFocus
-            onChange={(e) => {
-              // setContainers
-              setTaskTitle(e.target.value)
-              
-            }}
-            className="flex-grow bg-colBgColor h-20 rounded px-2 py-1 hover:border-rose-400 outline-none transition border border-transparent focus:border-rose-400"
-          />
-        </div>
-    );
-}
+        className="flex-grow bg-colBgColor h-20 rounded px-2 py-1 hover:border-rose-400 outline-none transition border border-transparent focus:border-rose-400"
+      />
+    </div>
+  );
+};
 
 export default TaskItem;
